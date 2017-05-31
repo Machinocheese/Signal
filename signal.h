fd49da416cb14be91f8cf662e853fda8e30d0b46
Machinocheese <zbill15@yahoo.com>
2017-05-31 17:46:09 -0400

    Added methods
    added getColorTable method and started on the LZW decompression method... (I still have to learn how it works)

--- signal.h
+++ signal.h
@@ -2,6 +2,8 @@
 #define SIGNAL_H
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <string>
 using namespace std;
 
 int exp(const int& num){
@@ -45,6 +47,114 @@ void print(vector<int> vec){
 	for(int i = 0; i < vec.size(); i++){
 		cout << vec[i];
 	}
+	cout << endl;
+}
+
+vector<int> getCodeStream(vector<uint8_t> input, int offset, int size){
+	int codeSize = input[offset] + 1; //minimum size for gifs
+	int clearCode, endCode, codeOffset = 0, counter, skippable, temp;
+	stringstream ss;
+	//cout << codeSize << endl;
+
+	vector<string> codeTable;
+	vector<int> codeStream;
+	vector<int> intermed;
+	vector<int> binaryInput;
+	for(int i = 0; i < size; i++){
+		ss << i;
+		codeTable.push_back(ss.str());
+		ss.str("");
+	}
+	clearCode = codeTable.size();
+	endCode = codeTable.size() + 1;
+	codeTable.push_back("4");
+	codeTable.push_back("5");
+
+	//this consumes a lot of memory, but makes the code easier to write/read
+	offset += 1;
+	skippable = input[offset] - 1;
+	offset += 1;
+	for(int i = offset; i < offset + skippable; i++){
+		intermed = hexToBin(input[i]);
+		for(int i = 0; i < 8; i++){
+			if(intermed.empty()) binaryInput.push_back(0);
+			else{
+				binaryInput.push_back(intermed.back());
+				intermed.pop_back();
+			}
+		}
+	}
+
+	intermed.clear();
+
+	while(!binaryInput.empty()){
+		counter = 0;
+		intermed = vector<int>(binaryInput.begin(), binaryInput.begin() + codeSize);
+		reverse(intermed.begin(), intermed.end());
+		temp = binToHex(intermed);
+		binaryInput.erase(binaryInput.begin(), binaryInput.begin() + codeSize);
+		codeStream.push_back(temp);
+		for(int i = 0; i < codeTable.size(); i++){
+			
+		}
+		//don't know what code is, so just fill in with # and position
+	}
+	
+////////////////////////////////////////////////loop based on hex number, calc all at once from one hex # in one loop
+
+	codeTable[clearCode] = "clear code";
+	codeTable[endCode] = "end of information code";
+	cout << "\nCode Table: \n------------------------\n";
+	for(int i = 0; i < codeTable.size(); i++){
+		printf("Position: %2d | ", i);
+		cout << codeTable[i] << endl;
+	}
+	print(codeStream);
+	return codeStream;
+}
+
+vector<string> getColorTable(vector<uint8_t> input){
+	vector<int> packed;
+	vector<string> colorTable;
+	stringstream ss;
+	int test = 0, a, b, c, counter = 0;
+	packed = hexToBin(input[10]);
+
+	if(packed[0] == 0)
+		return colorTable;
+
+	test = binToHex(vector<int>(packed.end() - 3, packed.end())); //last 3 bits
+	test = exp(test + 1); //there are 2^(N+1) colors
+	test *= 3; //each color has 3 bytes
+
+	for(int i = 13; i < 13 + test; i += 3){
+		a = input[i];
+		b = input[i+1];
+		c = input[i+2];
+		
+		if(a < 0x10) ss << 0;
+		ss << hex << a;
+		if(b < 0x10) ss << 0;
+		ss << hex << b;
+		if(c < 0x10) ss << 0;
+		ss << hex << c;
+		//this is code in an attempt to clean the color table
+		if(counter == 1 && ss.str() == "000000"){
+			ss.str("");
+			continue;
+		} 
+		colorTable.push_back(ss.str());
+		if(ss.str() == "000000")
+			counter = 1;
+		ss.str("");
+	}
+/*	make debugging easier
+	for(int i = 0; i < colorTable.size(); i++){
+		cout << colorTable[i] << endl;
+	}
+	cout << "SIZE: " << colorTable.size() << endl;
+*/
+	return colorTable;
 }
 
 int calculateOffset(vector<uint8_t> input){
@@ -79,7 +189,9 @@ int calculateOffset(vector<uint8_t> input){
 		}
 	}
 
-	cout << totalOffset;
-	printf("\n%x", input[totalOffset]);
+	//skip over Image Descriptor
+	totalOffset += 10;
+	cout << totalOffset << endl;
+	return totalOffset;
 }
 #endif
